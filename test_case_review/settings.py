a2bd252a422e5e318cdedf9baa837f14b6339ddc
Vladislav <75930878+ErshovVE@users.noreply.github.com>
2022-03-20 17:17:48 +0300

    Update settings.py

--- test_case_review/settings.py
+++ test_case_review/settings.py
@@ -124,5 +124,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = "/review/static"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
